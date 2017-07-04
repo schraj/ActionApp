9971946e8c9b1ea5ffa7b53c4dc4a3bc93e4c9c9
Jeremy Schrader <schraj@gmail.com>
2017-07-03 20:22:31 -0700

    update graphql server

--- RelayServer/server.js
+++ RelayServer/server.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import Koa from 'koa';
-import graphQLHTTP from 'koa-graphql';
-import mount from 'koa-mount';
-import convert from 'koa-convert';
+import graphQLHTTP from 'express-graphql';
 import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 
 // run the schema update
-import './scripts/updateSchema'
+import './scripts/updateSchema';
 
 import schema from './api/schema';
 
@@ -16,45 +13,34 @@ const APP_PORT = 3000;
 const GRAPHQL_PORT = 8080;
 
 // Expose a GraphQL endpoint
-const graphQLServer = new Koa();
-
-graphQLServer.use(mount('/', convert(graphQLHTTP({
-  schema,
-  pretty: true,
-  graphiql: true,
-}))));
-
+const graphQLServer = express();
+graphQLServer.use('/', graphQLHTTP({ schema, pretty: true,   graphiql: true, }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+    `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
 
 // Serve the Relay app
 const compiler = webpack({
-  devtool: 'cheap-module-eval-source-map',
-  entry: path.resolve(__dirname, 'webapp', 'app.js'),
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        test: /\.js$/,
-      },
-    ],
-  },
-  output: { filename: 'app.js', path: '/' },
+    entry: path.resolve(__dirname, 'webapp', 'app.js'),
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel',
+                test: /\.js$/,
+            },
+        ],
+    },
+    output: { filename: 'app.js', path: '/' },
 });
-
 const app = new WebpackDevServer(compiler, {
-  contentBase: '/public/',
-  proxy: { '/graphql': `http://localhost:${GRAPHQL_PORT}` },
-  publicPath: '/js/',
-  stats: { colors: true },
+    contentBase: '/public/',
+    proxy: { '/graphql': `http://localhost:${GRAPHQL_PORT}` },
+    publicPath: '/js/',
+    stats: { colors: true },
 });
-
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
-
 app.listen(APP_PORT, () => {
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
+    console.log(`App is now running on http://localhost:${APP_PORT}`);
 });
-
